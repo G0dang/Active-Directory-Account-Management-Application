e1fdc80a901cc9f58206ed461a6cfc8437d2045d
Aayush Gurung <Aayush.Gurung@detmoldgroup.com>
2024-06-06 14:30:36 +0930

    Created class that has a method which will search if the combobox items contains  a string. If yes, then it will select the item.

---
+++ UserMaker/Class/SearchComboBox.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserMaker.Class
+{
+	internal class SearchComboBox
+	{
+		public void SelectComboBoxItemContains(ComboBox comboBox, string searchMe)
+		{
+			// Loop through all items in the ComboBox
+			for (int i = 0; i < comboBox.Items.Count; i++)
+			{
+				// Get the current item as a string
+				string currentItem = comboBox.GetItemText(comboBox.Items[i]);
+
+				// Check if the current item contains the target substring
+				if (currentItem.Contains(searchMe, StringComparison.OrdinalIgnoreCase))
+				{
+					// Select the item if it contains the substring
+					comboBox.SelectedIndex = i;
+					return; 
+				}
+			}
+
+			MessageBox.Show($"Item containing '{searchMe}' not found in the ComboBox.");
+		}
+	}
+}
