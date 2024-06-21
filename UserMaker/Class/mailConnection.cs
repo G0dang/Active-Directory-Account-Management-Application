cd9f6033984079bde4e7625dc7196ebad951b9ed
Aayush Gurung <Aayush.Gurung@detmoldgroup.com>
2024-06-21 12:28:49 +0930

    - Removed "New-RemotemailBox" command and replaced it with "Enable-RemoteMailBox' - Modified powershell script - Asynchronous process established - Cancellation token implemented

--- UserMaker/Class/mailConnection.cs
+++ UserMaker/Class/mailConnection.cs
@@ -15,21 +15,23 @@ using System.Xml.Linq;
 using static System.Windows.Forms.VisualStyles.VisualStyleElement.Page;
 using static System.Windows.Forms.VisualStyles.VisualStyleElement.Spin;
 using System.Drawing.Text;
+using System.DirectoryServices;
+using UserMaker.Forms;
+using System.Threading;
 
 namespace UserMaker
 {
 	public class MailBoxConnect
 	{
-		public static void GetMailboxInfo(string userName, string password, string newPassword, string firstName, string lastName, string domain,string orgUnit)
+		//private ProgressBar pb = Application.OpenForms["detmoldApp"].Controls["progressBar_Mail"] as ProgressBar;
+		
+		
+		
+		public static async Task GetMailboxInfo(string userName, string password, string firstName, string lastName, string domain, CancellationToken cancelAsync)
 		{
 			// Configuration variables
 			string exchangeServer = "sgmail00.internal.detmold.com.au";
-																		
-			string fullName = $"{firstName} {lastName}";
-
-			
-
-			string exOUDN = "OU=Aayush Test,OU=Accounts,DC=internal,DC=detmold,DC=com,DC=au"; 
+			string strUserPrincipalName, strSamAccountName;
 			// Create a secure password
 
 			SecureString securePassword = new SecureString();
@@ -37,7 +39,7 @@ namespace UserMaker
 			{
 				securePassword.AppendChar(c);
 			}
-			
+
 			// Create credential object
 			PSCredential credential = new PSCredential($"{userName}", securePassword);
 
@@ -45,74 +47,133 @@ namespace UserMaker
 			Uri connectionUri = new Uri($"http://{exchangeServer}/PowerShell/");
 
 			WSManConnectionInfo connectionInfo = new WSManConnectionInfo(connectionUri, "http://schemas.microsoft.com/powershell/Microsoft.Exchange", credential);
-			
-		 
+
+
 			connectionInfo.AuthenticationMechanism = AuthenticationMechanism.Kerberos;
-				
 			
-			
-			
+
+			if (string.IsNullOrEmpty(lastName))
+			{
+				strUserPrincipalName = firstName + "@" + domain;
+				strSamAccountName = firstName;
+			}
+
+			else
+			{
+				strUserPrincipalName = firstName + "." + lastName + "@" + domain;
+				strSamAccountName = firstName + "." + lastName;
+			}
+
 			// Create runspace
 			using (Runspace rs = RunspaceFactory.CreateRunspace(connectionInfo))
 			{
-
-				rs.Open();
-				MessageBox.Show("Connection Established");
-
-				// Create PowerShell instance
-				PowerShell powershell = PowerShell.Create();
-				powershell.Runspace = rs;
-				
-				//string upn = string.IsNullOrEmpty(lastName) ? $"{firstName}@{domain}" : $"{firstName}.{lastName}@{domain}".Replace(" ", ".");
-				//if no lastname fname@domain, else fname.lname@domain
-				
-				//powershell.AddCommand(File.ReadAllText(@"C:\Users\Aayush.Gurung\OneDrive - Detmold Group\Desktop\script.ps1")).Invoke();
-
-				if (string.IsNullOrEmpty(lastName))
+				try
 				{
-					string upn = $"{firstName}@{domain}";
-					string script = "New-RemoteMailbox";
-					powershell.AddCommand(script);
-					powershell.AddParameter("Name", fullName);
-					powershell.AddParameter("FirstName", firstName);
-					powershell.AddParameter("$Password", newPassword); //newPassword is the password for new user mail
-					powershell.AddParameter("$Domain", domain);
-					powershell.AddParameter("$upn", upn);
-					powershell.AddParameter("OnPremisesOrganizationalUnit", exOUDN);
-					powershell.AddParameter("PrimarySmtpAddress", upn);
-				}
-				else
-				{
-					string upn = $"{firstName} {lastName}@{domain}".Replace (" ",".");
-					string script = "New-RemoteMailbox";
-					powershell.AddCommand(script);
-					powershell.AddParameter("Name", fullName);
-					powershell.AddParameter("FirstName", firstName);
-					powershell.AddParameter("LastName", lastName);
-					powershell.AddParameter("Password", securePassword);
-					powershell.AddParameter("Domain", domain);
-					powershell.AddParameter("UserPrincipalName", upn);
-					powershell.AddParameter("OnPremisesOrganizationalUnit", exOUDN);
-					powershell.AddParameter("PrimarySmtpAddress", upn);
-				}
-				//powershell.AddParameter("Anr", "Pay");
+					MessageBox.Show("Connection Established");
 
-				var results = powershell.Invoke();
+					await Task.Run(() => rs.Open(), cancelAsync);
 
-					//string outResult = $"Script executed successfully.\n\nHere is the list of result \n";
-					// Check for errors
-					if (powershell.HadErrors)
+					MessageBox.Show("Mailbox creation started");
+
+					using (DirectoryEntry entry = new DirectoryEntry("LDAP://internal.detmold.com.au"))
+					using (DirectorySearcher searcher = new DirectorySearcher(entry))
 					{
-						foreach (var errorRecord in results)
-						{
+						// Initialize DirectoryEntry and DirectorySearcher objects
+						searcher.Filter = "(userPrincipalName=" + strUserPrincipalName + ")";
+						SearchResult result = searcher.FindOne();
 
-							MessageBox.Show($"Error: {errorRecord.ToString()}");
+						if (result != null)
+						{
+							MessageBox.Show("User Found");
+
+							PowerShell powershell = PowerShell.Create();
+							powershell.Runspace = rs;
+
+							powershell.AddCommand("Enable-RemoteMailbox")
+									  .AddParameter("Identity", strUserPrincipalName)
+									  .AddParameter("RemoteRoutingAddress", $"{strSamAccountName}@detconnect.mail.onmicrosoft.com");
+
+
+							var results = await Task.Run(() =>
+							{
+								var invokeResults = powershell.Invoke();
+								if (cancelAsync.IsCancellationRequested)
+								{
+									cancelAsync.ThrowIfCancellationRequested();
+								}
+								return invokeResults;
+							}, cancelAsync);
+
+							if (powershell.HadErrors)
+							{
+								foreach (var errorRecord in powershell.Streams.Error)
+								{
+									MessageBox.Show($"Error: {errorRecord.ToString()}");
+								}
+								if (!cancelAsync.IsCancellationRequested)
+								{
+									MessageBox.Show("Errors occurred during mailbox creation. Stopping task.");
+									return;
+								}
+							}
+
+							MessageBox.Show($"Mail for the user: {strSamAccountName} created succesfully");
+							
+							if (powershell.HadErrors)
+							{
+								foreach (var errorRecord in results)
+								{
+									MessageBox.Show($"Error: {errorRecord.ToString()}");
+								}
+
+								if (!cancelAsync.IsCancellationRequested) //will stop the asyn task 
+								{
+									MessageBox.Show("Errors occurred during mailbox creation. Stopping task.");
+									return;
+								}
+															
+							}
+
+							if (!cancelAsync.IsCancellationRequested) //will stop the async task when user is found and the mailbox is created.
+							{
+								cancelAsync.ThrowIfCancellationRequested();
+							}
 						}
+
+						else
+
+						{
+							MessageBox.Show($"User: {strSamAccountName.Replace("."," ")} not found. Will try again in 30 seconds...");
+
+						}
+
+						rs.Close();
+						
 					}
+				}
+
+				catch (OperationCanceledException)
+				{
+					MessageBox.Show("Task was cancelled.");
+				}
+
+				catch (Exception ex)
+				{
+
+					MessageBox.Show($"Unexpected error: {ex.Message}");
 					rs.Close();
-					// Close the runspace
-				
+				}
+
+				finally
+				{
+					rs.Close();
+					rs.Dispose();
+					
+				}
+
 			}
+				
+			
 		}
 	}
 }
