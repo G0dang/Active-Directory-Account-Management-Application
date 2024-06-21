696f616ba9e72031ae7894e288f21003263ce7b4
Aayush Gurung <Aayush.Gurung@detmoldgroup.com>
2024-06-21 12:34:22 +0930

    - it will be checked if the user has a mailbox before calling the MailBoxConnection() function. - Will not initiate the async process if the mailbox is found. -

--- UserMaker/Forms/adminForm.cs
+++ UserMaker/Forms/adminForm.cs
@@ -9,41 +9,49 @@ using System.Management.Automation;
 using System.Management.Automation.Runspaces;
 using System.Security;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using UserMaker.Class;
 using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
-
+using System.Threading.Tasks;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using System.DirectoryServices;
 
 namespace UserMaker.Forms
 {
 	public partial class adminForm : Form
 	{
+		
 		private string firstName;
 		private string lastName;
 		private string domain;
-		private string orgUnit;
-		private string password;
-		private string newPassword;
+		private System.Threading.Timer syncTimer;  //kept getting ambigious reference error
+		string samName;
+		string upnName;
+		private System.Windows.Forms.ProgressBar progressBar;
+		private CancellationTokenSource stopAsync;
 
-		public adminForm(string newPassword, string firstName, string lastName, string domain, string orgUNit)
+		public adminForm(string firstName, string lastName, string domain, System.Windows.Forms.ProgressBar progressBar)
 		{
 			InitializeComponent();
+
+
 			this.firstName = firstName;
 			this.lastName = lastName;
-
 			this.domain = domain;
-			this.orgUnit = orgUnit;
+			this.progressBar = progressBar;
 
 			adminPassword.KeyDown += new KeyEventHandler(adminPassword_KeyDown); // used for initiating btn click action after typing password and hitiing 'Enter' key in admin login form.
-
-
-
+			
+		
 		}
 
-		
-		private void adminBtnOKClick(object sender, EventArgs e)
+
+		private async void adminBtnOKClick(object sender, EventArgs e)
 		{
+
+			var stopAsync = new CancellationTokenSource(); // to stop the async process when certain conditions are met
+			//string[] propertiesToLoad = { "targetAddress" };
+
 			if (string.IsNullOrEmpty(usernameBox.Text))
 			{
 				label_username.ForeColor = Color.OrangeRed;
@@ -64,11 +72,95 @@ namespace UserMaker.Forms
 			string userName = usernameBox.Text;
 			string password = adminPassword.Text;
 
+			if (string.IsNullOrEmpty(lastName))
+			{
+				upnName = firstName + "@" + domain;
+				samName = firstName;
+			}
 
-			MailBoxConnect.GetMailboxInfo(userName, password, newPassword, firstName, lastName, domain, orgUnit);
+			else
+			{
+				upnName = firstName + "." + lastName + "@" + domain;
+				samName = firstName + "." + lastName;
+			}
 
+			using (DirectoryEntry entry = new DirectoryEntry("LDAP://internal.detmold.com.au"))
+			using (DirectorySearcher searcher = new DirectorySearcher(entry))
+			{
+					searcher.Filter = "(targetAddress=" + "SMTP:" + samName + "@detconnect.mail.onmicrosoft.com)";
+					SearchResult routingResult = searcher.FindOne();
+					if (routingResult == null)
+					{
+					MessageBox.Show("MailBox not found. Next step: Creating Mailbox...");
+						try
+						{
+							syncTimer = new System.Threading.Timer(async _ =>
+							
+							{
+								if (stopAsync.Token.IsCancellationRequested)
+								{
+									return;
+								}
+								try
+								{
+									await MailBoxConnect.GetMailboxInfo(userName, password, firstName, lastName, domain, stopAsync.Token);
+								}
+								catch ( OperationCanceledException)
+								{
+									MessageBox.Show("Task was cancelled.");
+								}
+								catch (Exception ex)
+								{
+									MessageBox.Show($"Unexpected error: {ex.Message}");
+								}
+								finally
+								{
+									progressBar.Invoke((MethodInvoker)(() => progressBar.Visible = false));
+									
+									if (!stopAsync.IsCancellationRequested)
+									{
+										stopAsync.Cancel();
+									}
+								}
+							}, null, 0, 10000);
 
+							progressBar.Visible = true;
+						}
+
+						catch (Exception ex)
+						{ 
+							MessageBox.Show($"Unexpected error: {ex.Message}");
+							progressBar.Visible = false;
+						}
+
+					}
+
+								
+					else
+					{
+						MessageBox.Show($"User: {samName.Replace("."," ")} already has a mailbox");
+
+					}
+
+				
+
+				this.Close();
+			}
 		}
+
+		private void OnFormClosing(object sender, FormClosingEventArgs e)
+		{
+			if (syncTimer != null)
+			{
+				syncTimer.Dispose();
+			}
+			if (stopAsync != null)
+			{
+				stopAsync.Cancel();
+				stopAsync.Dispose();
+			}
+		}
+
 
 		private void adminBtnCancelClick(object sender, EventArgs e)
 		{
