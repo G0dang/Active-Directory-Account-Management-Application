e03e0b5439db9c358bbed2eef098aaa3fb6281d7
Aayush Gurung <Aayush.Gurung@detmoldgroup.com>
2024-06-25 12:49:44 +0930

    - Changed form naame

--- UserMaker/Program.cs
+++ UserMaker/Program.cs
@@ -11,7 +11,7 @@ namespace UserMaker
 			// To customize application configuration such as set high DPI settings or default font,
 			// see https://aka.ms/applicationconfiguration.
 			ApplicationConfiguration.Initialize();
-			Application.Run(new adminLogin());
+			Application.Run(new detmoldApp());
 		}
 	}
 }
